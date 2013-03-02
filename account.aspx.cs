fde23698dc61ff55cb7fc7fcd013323ea575d247
Joel Vasallo <joelvasallo@gmail.com>
2013-03-02 04:45:03 -0600

    Added account page, nothing fancy again but its only accessible if you are logged in.

---
+++ account.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PetFinder {
+    public partial class account : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            if (Session["loggedin"] == null) {
+                Response.Redirect("login.aspx");
+            }
+        }
+
+        public void btnLogout_Click(object sender, EventArgs e) {
+            Session["loggedin"] = null;
+            Response.Redirect("default.aspx");
+        }
+    }
+}
